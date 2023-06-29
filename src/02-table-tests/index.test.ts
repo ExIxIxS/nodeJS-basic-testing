5d8bf6b695b3ddd3c0312ad5f50384a0c67a5b11
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-06-29 13:45:48 +0200

    feat: add solutions for 02, 03 modules

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,29 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 10, b: 1, action: Action.Subtract, expected: 9 },
+  { a: 1.2, b: 1, action: Action.Subtract, expected: 1.2 - 1 },
+  { a: 10, b: 20, action: Action.Subtract, expected: -10 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 3, b: 0, action: Action.Multiply, expected: 0 },
+  { a: 9, b: 3, action: Action.Divide, expected: 3 },
+  { a: 9, b: 1, action: Action.Divide, expected: 9 },
+  { a: 0, b: 10, action: Action.Divide, expected: 0 },
+  { a: 10, b: 0, action: Action.Divide, expected: Infinity },
+  { a: 10, b: 0, action: Action.Exponentiate, expected: 1 },
+  { a: 10, b: 1, action: Action.Exponentiate, expected: 10 },
+  { a: 10, b: 2, action: Action.Exponentiate, expected: 100 },
+  { a: 100, b: 0.5, action: Action.Exponentiate, expected: 10 },
+  { a: '3', b: 2, action: Action.Add, expected: null },
+  { a: 3, b: '2', action: Action.Add, expected: null },
+  { a: 3, b: '2', action: 'invalid', expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('test case № %#', (testObj) => {
+    expect(simpleCalculator({ ...testObj })).toEqual(testObj.expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });

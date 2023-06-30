3eec4c261fed5c06c066fe0d68c7962ffecdb578
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-06-30 15:29:06 +0200

    refactor: refactor module 2

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -2,24 +2,12 @@ import { simpleCalculator, Action } from './index';
 
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 10, b: 1, action: Action.Subtract, expected: 9 },
-  { a: 1.2, b: 1, action: Action.Subtract, expected: 1.2 - 1 },
-  { a: 10, b: 20, action: Action.Subtract, expected: -10 },
-  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
-  { a: 3, b: 0, action: Action.Multiply, expected: 0 },
+  { a: 2, b: 3, action: Action.Multiply, expected: 6 },
   { a: 9, b: 3, action: Action.Divide, expected: 3 },
-  { a: 9, b: 1, action: Action.Divide, expected: 9 },
-  { a: 0, b: 10, action: Action.Divide, expected: 0 },
-  { a: 10, b: 0, action: Action.Divide, expected: Infinity },
-  { a: 10, b: 0, action: Action.Exponentiate, expected: 1 },
-  { a: 10, b: 1, action: Action.Exponentiate, expected: 10 },
-  { a: 10, b: 2, action: Action.Exponentiate, expected: 100 },
-  { a: 100, b: 0.5, action: Action.Exponentiate, expected: 10 },
-  { a: '3', b: 2, action: Action.Add, expected: null },
-  { a: 3, b: '2', action: Action.Add, expected: null },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
   { a: 3, b: '2', action: 'invalid', expected: null },
+  { a: '3', b: 2, action: Action.Add, expected: null },
 ];
 
 describe('simpleCalculator', () => {
